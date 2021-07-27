5f9252651963356a5e72835c8d4956a6016c472b
p0o0uya <pouyajamali@gmail.com>
2021-07-28 02:52:08 +0430

    Added the reqs class

---
+++ reqs.py
@@ -0,0 +1,43 @@
+import requests 
+import json
+
+class reqs:
+    
+    def __init__():
+        """Nothing to do yet"""
+        
+    def _get(url, params=None, headers=None):
+        """ Makes a Get Request """
+        retries = 3
+        for pj in range(0, retries):
+            try: 
+                response    = requests.get(url, params=params, headers=headers)
+                data        = json.loads(response.text)
+                return data
+            except Exception as e:
+                print("Exception occured when trying to get from " + url)
+                print(e)
+                data = {'code': -1, 'url':url, 'msg': e}
+        return data
+
+    def _post(url, params=None, headers=None):
+        """ Makes a Post Request """
+        try: 
+            response    = requests.post(url, params=params, headers=headers)
+            data        = json.loads(response.text)
+        except Exception as e:
+            print("Exception occured when trying to post to " + url)
+            print(e)
+            data = {'code': '-1', 'url':url, 'msg': e}
+        return data
+
+    def _delete(url, params=None, headers=None):
+        """ Makes a delete Request """
+        try: 
+            response    = requests.delete(url, params=params, headers=headers)
+            data        = json.loads(response.text)
+        except Exception as e:
+            print("Exception occured when trying to delete on " + url)
+            print(e)
+            data = {'code': '-1', 'msg':e}
+        return data
