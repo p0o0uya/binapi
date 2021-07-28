28acb4802b25155665422db786be88a3254be6e6
p0o0uya <pouyajamali@gmail.com>
2021-07-28 21:32:15 +0430

    Added files for package creation

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='binapi',
+    version='0.0',
+    author='Pouya Jamali',
+    author_email='pouyajamali@gmail.com',
+    description='A wrapper for binance futures API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/p0o0uya/binapi',
+    project_urls = {
+        "Bug Tracker": "https://github.com/p0o0uya/binapi/issues"
+    },
+    license='MIT',
+    packages=['binapi'],
+    install_requires=['requests', 'pandas'],
+)
