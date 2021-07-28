f8340da3dfe9302d2a74f043246316447e71d7f4
p0o0uya <pouyajamali@gmail.com>
2021-07-28 23:57:24 +0430

    version incremented

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='binapi',
-    version='0.2',
+    version='0.3',
     author='Pouya Jamali',
     author_email='pouyajamali@gmail.com',
     description='A wrapper for binance futures API',
